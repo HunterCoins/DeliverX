aeb4958ace32c9e2788c9ffa9b756e9bc0e4f190
Valentin <davidegeakintana@gmail.com>
2022-06-14 14:03:54 +0300

    Slider v2

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,16 @@
 
 $(document).ready(function(){
-    $('.slider__inner-1').slick({
-        autoplay: true,
-        speed: 4250,
-        autoplaySpeed: 0,
-        cssEase: 'linear',
-        vertical: true,
-        slidesToShow: 2,
-        infinite: true,
-        arrows: false,
-        centerMode: true,
-        pauseOnHover: false,
-        draggable: false, 
-    });
-
-    $('.slider__inner-2').slick({
+    $('.slider__inner-2, .slider__inner-1').slick({
         autoplay: true,
         speed: 4500,
         autoplaySpeed: 0,
         cssEase: 'linear',
         vertical: true,
-        slidesToShow: 2,
+        slidesToShow: 3,
         infinite: true,
         arrows: false,
         centerMode: true,
-        pauseOnHover: false, 
+        pauseOnHover: false,
         draggable: false, 
     });
   });
